1c1f0a8c7fe45e4aac1c89caa7775c6549f58f5b
Joseph Fallait <31420570+fallait@users.noreply.github.com>
2020-11-03 00:26:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+import ScatterPlot from './ScatterPlot.js';
+import WorldMap from './WorldMap.js';
+d3.csv('cleanData.csv', d3.autoType).then(data=>{
+    d3.json("world-110m.json").then(world=>{
+        const cleanData = data;
+        const map = world;
+        const mapVis = WorldMap(map, data);
+        var scatterData = data.filter(d => d.time === 2014).filter(d => d.development !== null).filter(d => d.agriculturePercentGDP != "..").filter(d => d.manufacturingPercentGDP != "..").filter(d => d.industryPercentGDP != "..");
+        const scatter = ScatterPlot("#scatter", scatterData);
+        scatter.update(scatterData);
+            })})
