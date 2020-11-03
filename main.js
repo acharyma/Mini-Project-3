e38026fc10de3df8a967b4db8878734408380cf5
Joseph Fallait <31420570+fallait@users.noreply.github.com>
2020-11-02 23:53:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import scatterPlot from './scatterPlot.js';
+import ScatterPlot from './ScatterPlot.js';
 import WorldMap from './WorldMap.js';
 d3.csv('cleanData.csv', d3.autoType).then(data=>{
     d3.json("world-110m.json").then(world=>{
@@ -6,6 +6,6 @@ d3.csv('cleanData.csv', d3.autoType).then(data=>{
         const map = world;
         const mapVis = WorldMap(map, data);
         var scatterData = data.filter(d => d.time === 2014).filter(d => d.development !== null).filter(d => d.agriculturePercentGDP != "..").filter(d => d.manufacturingPercentGDP != "..").filter(d => d.industryPercentGDP != "..");
-        const scatter = scatterPlot("#scatter", scatterData);
+        const scatter = ScatterPlot("#scatter", scatterData);
         scatter.update(scatterData);
             })})
